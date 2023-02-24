6bfae56b1145d831dbe2ed50b7a5ae563415cee0
navnithi <navnithi@gmail.com>
2023-02-24 14:58:33 +0100

    card added

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -4,13 +4,15 @@ const Movie = (props) => {
     const { Title, Year, Type, Poster } = props.movie;
     console.log(Title);
     return (
-        <div className="movie">
-            <img src={Poster} alt="movie" />
-            <section className="info">
-                <p>Title: {Title}</p>
-                <p>Type: {Type}</p>
-                <p>Year: {Year}</p>
-            </section>
+        <div className="card_container">
+            <div className="image_conntainer">
+                <img src={Poster} alt="movie" />
+                <section className="card_info">
+                    <p>Title: {Title}</p>
+                    <p>Type: {Type}</p>
+                    <p>Year: {Year}</p>
+                </section>
+            </div>
         </div>
     );
 };
